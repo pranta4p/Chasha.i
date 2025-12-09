4f3a4c22a0f7b644ef10f0083d9ebc58ffef9d24
tanvir <aliashraftanvir410@gmail.com>
2025-12-09 16:21:40 +0600

    Email verification updated with brevo Api

--- server/config/email.js
+++ server/config/email.js
@@ -1,32 +1,47 @@
 const nodemailer = require("nodemailer");
 const axios=require('axios')
-const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com", // SMTP Server
-  port: 587,                    // Port
-  secure: false,                // false for 587
-  auth: {
-    user: process.env.EMAIL_USER, // Login (provided by Brevo)
-    pass: process.env.BREVO_API_KEY      // SMTP key you generated
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp-relay.brevo.com", // SMTP Server
+//   port: 587,                    // Port
+//   secure: false,                // false for 587
+//   auth: {
+//     user: process.env.EMAIL_USER, // Login (provided by Brevo)
+//     pass: process.env.BREVO_API_KEY      // SMTP key you generated
+//   },
+//   connectionTimeout: 10000,  // 10 sec
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000
+// });
 
-async function sendVerificationEmail(toEmail, verificationLink,code) {
-  const mailOptions = {
-    from: `"Chasha.i" <${process.env.EMAIL}>`, // must match Login
-    to: toEmail,
-    subject: "Verify your email",
-    html: `<p>Click the link to verify your email:</p>
-           <a href="${verificationLink}">${verificationLink}</a>
-           <br><p>Or use this code :<h1> ${code} </h1></p>`
-  };
+async function sendVerificationEmail(toEmail, verificationLink, code) {
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: { name: "Chasha.i", email: process.env.EMAIL },
+        to: [{ email: toEmail }],
+        subject: "Verify your email",
+        htmlContent: `
+          <p>Click to verify your email:</p>
+          <a href="${verificationLink}">${verificationLink}</a>
+          <br><br>
+          <p>Your code: <strong>${code}</strong></p>
+        `
+      },
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log("Email sent: " + info.messageId);
-    return info;
+    console.log("Email API resp:", response.data);
+
   } catch (err) {
-    console.error("Error sending email:", err);
+    console.error("Brevo API Error:", err.response?.data || err.message);
   }
 }
+
 
 module.exports = sendVerificationEmail;
